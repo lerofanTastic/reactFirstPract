6278c8f0d14ec8569ecd2bae878b125f99e2ddb8
lerofanTastic <leroty@bk.ru>
2025-04-20 19:13:46 +0800

    someRoflsList

--- src/components/AnimeList/AnimeList.jsx
+++ src/components/AnimeList/AnimeList.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./AnimeList.module.css";
 
 export const AnimeList = ({ animeList }) => {
-
   return (
-    <div style={{display: 'flex', flexDirection: 'column'}}>
-        {
-            animeList.map(anime => <Link to={`${anime.id}`}>{anime.title}</Link>)
-        }
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        {animeList.map((anime) => (
+          <Link to={`${anime.id}`}>
+            <img className={styles.img} src={anime.image} />
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
